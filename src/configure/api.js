e84cbcb17e06675b9c75ca3bf6db8146ed7716c3
Travel023 <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2025-04-06 15:54:32 +0530

    fix bug

--- src/configure/api.js
+++ src/configure/api.js
@@ -1,2 +1,2 @@
-export const userApi="http://localhost:3001/"
-export const adminApi="http://localhost:3001/admin"
+export const userApi="https://skyblumes-backend.onrender.com/"
+export const adminApi="https://skyblumes-backend.onrender.com/admin"
