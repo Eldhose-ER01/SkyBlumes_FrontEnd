b8f3b43906edaa76e5d23b1e815b41147f3b9cd7
Travel023 <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2025-04-08 21:05:11 +0530

    fix it

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 
-const clientId = import.meta.env.VITE_REACT_APP_CLIENT_ID; // use VITE_ prefix for Vite projects
+const clientId ="172635673249-28l59cndnruhbkchsultcj9ci86hftn8.apps.googleusercontent.com" // use VITE_ prefix for Vite projects
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
