17421d7ab1df43aecb9d5df9b66632ac827a472f
Travel023 <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2025-04-08 20:21:02 +0530

    fix it

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import {Provider} from"react-redux"
 import store from './redux/store.js'
-const clientId = '401197160662-0djt65t1o9amj3om40iaj4ikgidk2hu7.apps.googleusercontent.com';
+const clientId = '172635673249-28l59cndnruhbkchsultcj9ci86hftn8.apps.googleusercontent.com';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
